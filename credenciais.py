8a74fc0034676e9f696eab16ec9b93366776c5c9
Everson <eversoncoutinho@gmail.com>
2020-10-14 02:45:25 -0300

    add a url no arquivo credenciais

--- credenciais.py
+++ credenciais.py
@@ -1,3 +1,4 @@
 class Usuario():
     username = 'profeversoncoutinho@gmail.com'
     password = 'ev310931'
+    url = 'https://www.instagram.com/'
--- entrada.py
+++ entrada.py
@@ -10,17 +10,15 @@ tar xzf arquivo.tar.gz
 sudo mv geckodriver /usr/bin/geckodriver 
 '''
 
-url = 'https://www.instagram.com/'
-
 options = Options()
 options.headless = True
 browser = webdriver.Firefox(options=options) #acessar o firefox
 browser.implicitly_wait(5) #espera 5 s para carregar qualquer coisa
-browser.get(url) #acessar a url
+browser.get(Usuario.url) #acessar a url
 #login_link = browser.find_element_by_xpath("//a[text()='Log in']"#encontra o elemento <a> cujo texto é igual a Log in suando xPath.
 #login_link.click()
 
-sleep(3)
+sleep(2)
 username_input = browser.find_element_by_css_selector("input[name='username']") #encontra o username input pelo css. 
                                                                                 #Verifique o name inspecionando a página
 
@@ -36,7 +34,6 @@ login_button = browser.find_element_by_xpath("//button[@type='submit']")
 login_button.click()
 
 nao_ativar = browser.find_elements_by_xpath("//button[@class='aOOlW HoLwm']")
-
 
 browser.close()
 print("Fim do programa")
